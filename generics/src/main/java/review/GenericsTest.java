5d6ace04d4585ace929c9bb2dfd50fe68f16c521
reyhaneterzioglu <terzioglureyhane@gmail.com>
2024-02-08 03:23:45 +0100

    GenericsTest class added

--- generics/src/main/java/review/GenericsTest.java
+++ generics/src/main/java/review/GenericsTest.java
@@ -21,10 +21,32 @@ public class GenericsTest {
         teacherList.add(new Teacher("Eric", 201));
         teacherList.add(new Teacher("David", 202));
 
+        printInfo(studentList);
+//        printInfo(teacherList);
     }
 
 
     // write a method that takes a list of students and prints each student object and total count of students
 
+    public static void printInfo(List<Student> studentList){
+
+        for (Student student : studentList) {
+
+            System.out.println(student);
+        }
+
+        System.out.println(studentList.size());
+
+    }
+
+    public static void printInfo2(List<Teacher> teacherList){
+
+        for (Teacher teacher : teacherList) {
+
+            System.out.println(teacher);
+        }
+
+        System.out.println(teacherList.size());
+    }
 
 }
