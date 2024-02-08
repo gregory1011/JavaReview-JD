caf5b474b32ce093ebeae5fd6e89536ffc12945b
reyhaneterzioglu <terzioglureyhane@gmail.com>
2024-02-08 04:11:04 +0100

    using bounded types

--- generics/src/main/java/review/MyUtils.java
+++ generics/src/main/java/review/MyUtils.java
@@ -1,8 +1,9 @@
 package review;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class MyUtils<T> {
+public class MyUtils<T extends Person> {
 
     public void printInfo3(List<T> personList) {
 
@@ -20,18 +21,30 @@ public class MyUtils<T> {
         return personList.get(personList.size() - 1);
     }
 
+
     // write a method that takes a list of objects and prints everyone's names one by one
 
+    public void printNames(List<T> personList) {
 
+        for (T person : personList) {
 
-
+            System.out.println(person.getName());
+        }
+    }
 
 
     public static void main(String[] args) {
 
-//        MyUtils<Student> myUtils = new MyUtils<>();
-//
-//        myUtils.printInfo3();
+        List<Student> studentList = new ArrayList<>();
+
+        studentList.add(new Student("Mike", 101));
+        studentList.add(new Student("Joe", 102));
+        studentList.add(new Student("Max", 103));
+
+        MyUtils<Student> myUtils = new MyUtils<>();
+
+        myUtils.printNames(studentList);
+        myUtils.printInfo3(studentList);
 
 //        MyUtils.returnLastElement()
     }
